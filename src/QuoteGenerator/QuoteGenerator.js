7a2448d787685be3891ba7b12c6b5c091efad290
Keegan Gauthier <Keegan.gauthier@resmed.com>
2024-04-10 15:44:01 -0300

    added copy to clipboard button

--- src/QuoteGenerator/QuoteGenerator.js
+++ src/QuoteGenerator/QuoteGenerator.js
@@ -5,7 +5,8 @@ const QuoteGenerator = () => {
   const [randomQuote, setRandomQuote] = useState('');
   const [typedQuote, setTypedQuote] = useState('');
   const [isTyping, setIsTyping] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // New state variable
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   const generateRandomQuote = async () => {
     setIsButtonDisabled(true); // Disable the button when quote generation starts
@@ -20,7 +21,6 @@ const QuoteGenerator = () => {
     try {
       const response = await fetch(url, options);
       const result = await response.text();
-      console.log(result)
       setRandomQuote(result);
 
       setIsTyping(true);
@@ -29,28 +29,55 @@ const QuoteGenerator = () => {
         setTimeout(() => {
           setTypedQuote((prev) => prev + result[i]);
           if (i === result.length - 1) {
-            setIsButtonDisabled(false); // Enable the button when quote has been fully typed out
+            setIsButtonDisabled(false);
+            setIsTyping(false);
           }
         }, i * 50);
       }
     } catch (error) {
       console.error('Error fetching quote:', error);
-      setIsButtonDisabled(false); // Enable the button in case of error
+      setIsButtonDisabled(false);
+      setIsTyping(false);
+    }
+  };
+
+  const copyToClipboard = () => {
+    if (randomQuote && !isAnimating) {
+      setIsAnimating(true);
+      navigator.clipboard.writeText(randomQuote).then(function() {
+        setTimeout(() => {
+          setIsAnimating(false); // Animation ends here
+        }, 3000);
+        var copyButton = document.querySelector('.copy-button');
+        copyButton.classList.add('clicked');
+        setTimeout(function() {
+          copyButton.classList.remove('clicked');
+        }, 3000);
+      }, function(err) {
+        console.error('Could not copy text: ', err);
+        setIsAnimating(false); // In case of error, also end the animation
+      });
     }
   };
 
   return (
     <div className="quote-container">
-      <button className="quote-button" onClick={generateRandomQuote} disabled={isButtonDisabled}>
-        Get Random Quote
-      </button>
+      <div className="button-container">
+        <button className="quote-button" onClick={generateRandomQuote} disabled={isButtonDisabled}>
+          Get Random Quote
+        </button>
+        <button className="copy-button" onClick={copyToClipboard} disabled={!randomQuote || isTyping || isAnimating}>
+          <i class="fas fa-clipboard"></i>
+          <i class="fas fa-check"></i>
+        </button>
+      </div>
       {isTyping ? <p className="quote">{typedQuote}</p> : null}
       {randomQuote && !isTyping && (
         <div className="quote">
           <p>{randomQuote}</p>
         </div>
       )}
-    </div>
+  </div>
   );
 };
 
